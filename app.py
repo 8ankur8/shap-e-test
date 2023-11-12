c72cb2b2430ca4b7fc94b41a57199fdc7ca184b7
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2023-11-12 12:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,27 @@ from app_image_to_3d import create_demo as create_demo_image_to_3d
 from app_text_to_3d import create_demo as create_demo_text_to_3d
 from model import Model
 
-DESCRIPTION = '# [Shap-E](https://github.com/openai/shap-e)'
+DESCRIPTION = "# [Shap-E](https://github.com/openai/shap-e)"
 
-if (SPACE_ID := os.getenv('SPACE_ID')) is not None:
-    DESCRIPTION += f'\n<p>For faster inference without waiting in queue, you may duplicate the space and upgrade to GPU in settings. <a href="https://huggingface.co/spaces/{SPACE_ID}?duplicate=true"><img style="display: inline; margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate Space" /></a></p>'
 if not torch.cuda.is_available():
-    DESCRIPTION += '\n<p>Running on CPU 🥶 This demo does not work on CPU.</p>'
+    DESCRIPTION += "\n<p>Running on CPU 🥶 This demo does not work on CPU.</p>"
 
 model = Model()
 
-with gr.Blocks(css='style.css') as demo:
+with gr.Blocks(css="style.css") as demo:
     gr.Markdown(DESCRIPTION)
+    gr.DuplicateButton(
+        value="Duplicate Space for private use",
+        elem_id="duplicate-button",
+        visible=os.getenv("SHOW_DUPLICATE_BUTTON") == "1",
+    )
     with gr.Tabs():
-        with gr.Tab(label='Text to 3D'):
+        with gr.Tab(label="Text to 3D"):
             create_demo_text_to_3d(model)
-        with gr.Tab(label='Image to 3D'):
+        with gr.Tab(label="Image to 3D"):
             create_demo_image_to_3d(model)
-demo.queue(api_open=False, max_size=5).launch()
+
+if __name__ == "__main__":
+    demo.queue(max_size=10).launch()
+
+# demo.queue(api_open=False, max_size=5).launch()
